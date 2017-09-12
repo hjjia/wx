32158b3224ef15466ccad10e441fa4169a895e77
Jia <normal999@163.com>
2017-09-12 23:32:46 +0800

    Add files via upload

--- pages/game/index.js
+++ pages/game/index.js
@@ -1,54 +1,61 @@
-//index.js
-//获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    currentNumber: 0,
+    prevNumber: 0,
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+  numSelect: function (event) {
+    console.log(event, 'event');
+    const inputNumber = event.currentTarget.dataset.num;
+    const { prevNumber, currentNumber } = this.data;
+    const _self = this;
+
+    if (inputNumber === '空格') {
+      if ((prevNumber * 1 + 1) === currentNumber * 1) {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          prevNumber: currentNumber,
+          currentNumber: ''
+        });
+      } else {
+        wx.showModal({
+          title: 'Game Over',
+          content: '游戏结束，您的成绩为' + prevNumber,
+          confirmText: '重新开始',
+          cancelColor: '结束',
+          success: function (status) {
+            console.log(status, 'st')
+            if (status.confirm) {
+              _self.setData({
+                prevNumber: 0,
+                currentNumber: 0
+              })
+            } else {
+              wx.switchTab({
+                url: '../index/index',
+              });
+              _self.setData({
+                prevNumber: 0,
+                currentNumber: 0
+              })
+              console.log('hello');
+            }
+          },
+          fail: function () {
+            
+          }
         })
       }
+    } else if (inputNumber === '占位符') {
+
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+      this.setData({
+        currentNumber: currentNumber * 1 === 0 ? inputNumber : currentNumber + '' + inputNumber
+      });
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
   }
 })
