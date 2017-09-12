42a68090838ade5287051192ae92981ed4c6905c
huangjiajia <huangjj@ecqun.com>
2017-09-12 20:10:51 +0800

    add method

--- pages/game/index.js
+++ pages/game/index.js
@@ -3,15 +3,33 @@ const app = getApp()
 Page({
   data: {
     currentNumber: 0,
+    prevNumber: 0,
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   numSelect: function (event) {
     console.log(event, 'event');
-    const currentNumber = event.currentTarget.dataset.num;
-    this.setData({
-      currentNumber:currentNumber
-    })
+    const inputNumber = event.currentTarget.dataset.num;
+    const { prevNumber, currentNumber } = this.data;
+
+    if (inputNumber === '空格') {
+      if ((prevNumber * 1 + 1) === currentNumber * 1) {
+        this.setData({
+          prevNumber: currentNumber,
+          currentNumber: ''
+        });
+      } else {
+        alert('game over');
+        return;
+      }
+    } else if (inputNumber === '占位符') {
+
+    } else {
+      this.setData({
+        currentNumber: currentNumber * 1 === 0 ? inputNumber : currentNumber + '' + inputNumber
+      });
+    }
+    
   }
 })
