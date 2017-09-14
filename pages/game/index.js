ca55bcf9c338f68984ee909123c6f5df0af54e1e
Jia <normal999@163.com>
2017-09-15 00:04:34 +0800

    Add files via upload

--- pages/game/index.js
+++ pages/game/index.js
@@ -3,16 +3,26 @@ let clockTimer = null;
 
 Page({
   data: {
-    currentNumber: 0,
-    prevNumber: 0,
-    tempNumber: 0,
-    begin: false,
-    gameTime: '00:00:00',
+    currentNumber: 0, // 当前输入的数字
+    prevNumber: 0, // 前一个输入的数字
+    tempNumber: 0, // 用来帮助显示当前输入数字
+    begin: false,  // 游戏开始
+    gameTime: '00:00:00', // 游戏计时
+    maxGrade: 0, // 最高分
+    showOperation: false,
+    showModal: false,
+    modal: {title: '提示', content: ['hahhha', 'hhhhhhhh']},
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  numSelect: function (event) {
+  container: function (event) {
+    const { showOperation } = this.data;
+    this.setData({
+      showOperation: false
+    })
+  },
+  numSelect: function (event) { // 点击键盘时
     console.log(event, 'event');
     const inputNumber = event.currentTarget.dataset.num;
     const { prevNumber, currentNumber, tempNumber, begin } = this.data;
@@ -22,21 +32,25 @@ Page({
       return;
     }
 
-    if (inputNumber === '空格') {
-      if ((prevNumber * 1 + 1) === currentNumber * 1) {
+    if (inputNumber === '空格') { // 点击空格结束当前的数字输入，判断是否结束
+      if ((prevNumber * 1 + 1) === currentNumber * 1) {  // 如果上一个数字 + 1 = 当前输入，继续
         this.setData({
           prevNumber: currentNumber,
           tempNumber: 0,
           // currentNumber: ''
         });
-      } else {
+      } else { // 不等于，游戏结束
         wx.showModal({
           title: 'Game Over',
           content: '游戏结束，您的成绩为' + prevNumber,
           confirmText: '重新开始',
           cancelColor: '结束',
           success: function (status) {
-            console.log(status, 'st')
+            // 清除计时器
+            clearInterval(clockTimer);
+
+            // 设置最高分
+            const maxGrade = setMaxGrade(prevNumber);
             if (status.confirm) {
             } else {
               wx.switchTab({
@@ -46,11 +60,13 @@ Page({
               console.log('hello');
             }
 
+            // 重置信息
             _self.setData({
               prevNumber: 0,
               currentNumber: 0,
               tempNumber: 0,
-              begin: false
+              begin: false,
+              maxGrade: maxGrade
             })
           },
           fail: function () {
@@ -99,15 +115,55 @@ Page({
       }, 1000)
     }
   },
-  overBtn: function (ebent) {
-    const { begin } = this.data;
+  overBtn: function (event) {
+    const { begin, prevNumber } = this.data;
 
     if (begin) {
+      const maxGrade = setMaxGrade(prevNumber);
       this.setData({
-        begin: false
+        begin: false,
+        maxGrade: maxGrade
       });
 
       clearInterval(clockTimer);
     }
+  },
+  settingBtn: function (event) {
+    event.st
+    const { showOperation } = this.data;
+    this.setData({
+      showOperation: !showOperation
+    })
+  },
+  operation: function (event) {
+    const { showModal } = this.data;
+    const title = showModal ? '提示' : '操作说明';
+    const content = showModal ? [] : [
+      '1. 只能从1开始',
+      '2. 按空格键结束当前数字输入'
+    ]
+    this.setData({
+      showModal: !showModal,
+      modal: {title, content}
+    })
+  },
+  modalBg: function (event) {
+    const { showModal } = this.data;
+    this.setData({
+      showModal: !showModal
+    })
   }
 })
+
+// 设置最高分
+function setMaxGrade(newGrade) {
+  const maxGrade = wx.getStorageSync('maxGrade');
+  
+  console.log(maxGrade, 'maxGrade');
+  if (!maxGrade || newGrade * 1 > maxGrade * 1) {
+    wx.setStorageSync('maxGrade', newGrade);
+    return newGrade;
+  }
+
+  return maxGrade;
+}
